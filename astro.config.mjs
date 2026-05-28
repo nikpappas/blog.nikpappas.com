c8be5c8db312acad47d60727b3258c5e41f6a59d
Nikolaos Pappas <nikolaospappa@gmail.com>
2026-05-28 13:28:59 +0300

    Fix deps and add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: 'https://blog.nikpappas.com',
+  integrations: [sitemap()],
 });
